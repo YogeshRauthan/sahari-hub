98dd31c7ba02e30efe61cdc5ab8d10d40eadba8f
YogeshRauthan <yogeshrauthan1998@gmail.com>
2024-07-14 06:26:27 +0530

    whatsapp number

--- src/app/components/WhatsappButton.jsx
+++ src/app/components/WhatsappButton.jsx
@@ -4,9 +4,12 @@ import React from 'react';
 import { FaWhatsapp } from 'react-icons/fa';
 
 const WhatsAppButton = () => {
+  // Format the phone number with country code
+  const phoneNumber = "918750709787"; // Assuming 91 is the country code for India
+
   return (
     <a
-      href="https://wa.me/8750709787"
+      href={`https://wa.me/${phoneNumber}`}
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 bg-green-500 text-white p-3 rounded-full shadow-lg shadow-gray-500 hover:bg-green-600 transition-colors duration-300 z-50"
